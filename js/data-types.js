c7364d932e81300676eda2713f0075982dd79cc3
NikolayGerasimov <kolyagerich@gmail.com>
2021-04-06 19:31:57 +0300

    js

---
+++ js/data-types.js
@@ -0,0 +1,20 @@
+'use strict'
+// однострочный комментарий
+
+/* многострочный
+комментарий */
+
+//отладка
+console.log ('Данные для отладки');
+
+// переменные
+// объявление переменных
+let age = 26;
+age = 27;
+console.log(age);
+
+let login = 'qwerty', password = 'qwerty123';
+let height = 400;
+let width = 700;
+
+let active, closed = 1;
